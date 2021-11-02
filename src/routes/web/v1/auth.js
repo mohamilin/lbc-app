69bce07df4d741eef30283b84151306ecd747eb8
Moh Amilin <amilinmoh99@gmail.com>
2021-11-02 21:49:32 +0700

    add : route homepage and register

--- src/routes/web/v1/auth.js
+++ src/routes/web/v1/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { register, login } = require('../../../controllers/web/auth');
+const { viewHomepage } = require('../../../controllers/web/homepage');
 
 const router = express.Router();
-router.get('/', register);
-
+router.get('/', viewHomepage);
+router.get('/register', register);
 router.get('/login', login);
 module.exports = router;
