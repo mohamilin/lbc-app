1d54d272b232c40a8f69bdb6d426c834b7514ae9
Moh Amilin <amilinmoh99@gmail.com>
2021-11-02 11:37:14 +0700

    add : web controlle student

---
+++ src/controllers/web/students.js
@@ -0,0 +1,19 @@
+const viewClass = async (req, res) => {
+  try {
+    res.render('pages/kelas_saya', { title: 'Kelas Saya' });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const viewDetailClass = async (req, res) => {
+  try {
+    res.render('pages/detail_kelas', { title: 'Kelas Saya 1' });
+  } catch (error) {
+    console.error(error);
+  }
+};
+module.exports = {
+  viewClass,
+  viewDetailClass,
+};
