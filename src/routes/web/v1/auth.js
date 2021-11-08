3d140538c56c5236c31fd95ae191d9d4db74cce0
Moh Amilin <amilinmoh99@gmail.com>
2021-11-08 20:46:08 +0700

    add : controller login and register

--- src/routes/web/v1/auth.js
+++ src/routes/web/v1/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { register, login } = require('../../../controllers/web/auth');
+const { register, login, actionRegister, actionLogin } = require('../../../controllers/web/auth');
 const { viewHomepage } = require('../../../controllers/web/homepage');
 
 const router = express.Router();
 router.get('/', viewHomepage);
 router.get('/register', register);
+router.post('/action/register', actionRegister);
 router.get('/login', login);
+router.post('/action/login', actionLogin);
+
 module.exports = router;
