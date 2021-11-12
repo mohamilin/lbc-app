1de9872dc27c5c1dd1747c0c1fc3bd43828b321c
Moh Amilin <amilinmoh99@gmail.com>
2021-11-13 05:06:46 +0700

    add : student controller

--- src/controllers/web/students.js
+++ src/controllers/web/students.js
@@ -1,20 +1,33 @@
-const viewClass = async (req, res) => {
+const { classService, transactionService } = require('../../services');
+const isSucces = require('../../helpers/transaction');
+
+const viewClassStudent = async (req, res) => {
   try {
-    const { name, role } = req.session.user;
-    res.render('pages/kelas_saya', { title: 'Kelas Saya', name, role });
+    const dataUser = req.session.user;
+
+    const dataClass = await transactionService.classTransaction(dataUser.id);
+    console.log('dataClass', dataClass);
+    res.render('pages/kelas_saya', { title: 'Kelas Saya', name: dataUser.name, role: dataUser.role, dataClass });
   } catch (error) {
     console.error(error);
   }
 };
 
-const viewDetailClass = async (req, res) => {
+const viewDetailClassStudent = async (req, res) => {
   try {
-    res.render('pages/detail_kelas', { title: 'Kelas Saya 1' });
+    const dataUser = req.session.user;
+    const { id } = req.params;
+    const dataClass = await classService.viewClassById(id);
+    const checkSuccess = await isSucces(dataUser, id);
+    if (checkSuccess.status !== 'Sukses') return res.redirect(`/transaction/class/${id}`);
+    console.log('dataClass', dataClass.name);
+    console.log('dataClass.chapterId[0].lessonId.title', dataClass.chapterId[1]);
+    res.render('pages/detail_kelas', { title: `${dataClass.name}`, name: dataUser.name, role: dataUser.role, dataClass });
   } catch (error) {
     console.error(error);
   }
 };
 module.exports = {
-  viewClass,
-  viewDetailClass,
+  viewClassStudent,
+  viewDetailClassStudent,
 };
