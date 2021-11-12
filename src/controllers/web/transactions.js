9cd5b62f9a5a1c3272e5122b20112b3eb82b5ddf
Moh Amilin <amilinmoh99@gmail.com>
2021-11-13 05:07:01 +0700

    add : transaction controller

--- src/controllers/web/transactions.js
+++ src/controllers/web/transactions.js
@@ -1,10 +1,14 @@
-const { classService } = require('../../services');
+const { classService, transactionService } = require('../../services');
 
 const viewTransaction = async (req, res) => {
   try {
     const { id } = req.params;
     const { user } = req.session;
     const classes = await classService.viewClassById(id);
+    const isSubscribe = await transactionService.isSubscribe({ id });
+    if (!isSubscribe) {
+      await transactionService.registerClass({ user, classes });
+    }
     const { price } = classes;
     const dataPrice = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(price);
     res.render('transaction', { user, classes, dataPrice });
@@ -13,6 +17,11 @@ const viewTransaction = async (req, res) => {
   }
 };
 
+const isSubscribe = async (req, res) => {
+  // const
+  const classSubscribe = await transactionService.isSubscribe();
+};
 module.exports = {
   viewTransaction,
+  isSubscribe,
 };
